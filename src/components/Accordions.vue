ceaf1788664588101e9c629c2fad09ac7afe955a
m19e <nexus.me7290@gmail.com>
2020-10-14 16:02:56 +0900

    Doesnt receive arg

--- src/components/Accordions.vue
+++ src/components/Accordions.vue
@@ -1,7 +1,7 @@
 <template>
     <div style="padding: 24px">
         <!-- 一括開閉ボタン -->
-        <button @click="toggleAll(showAll)">{{ showAll ? "Collapse" : "Expand" }} All</button>
+        <button @click="toggleAll()">{{ showAll ? "Collapse" : "Expand" }} All</button>
         <!-- アコーディオンのデータリストをv-forで順番に展開する -->
         <!-- listからacc(アコーディオン)とi(インデックス)を同時に取り出す -->
         <div v-for="(acc, i) in list" :key="i">
@@ -36,11 +36,14 @@ export default Vue.extend({
         };
     },
     methods: {
+        // アコーディオンのインデックスを受け取って開閉する
         toggle(i: number) {
             this.list[i].show = !this.list[i].show;
         },
-        toggleAll(cur: boolean) {
-            this.list = this.list.map((item) => Object.assign({}, item, { show: !cur }));
+        // リスト内の全てのshowを!showAllに置き換えて一括開閉する
+        // showAllを切り替える
+        toggleAll() {
+            this.list = this.list.map((item) => Object.assign({}, item, { show: !this.showAll }));
             this.showAll = !this.showAll;
         },
     },
